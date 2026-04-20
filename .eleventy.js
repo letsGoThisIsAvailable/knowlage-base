b04ac66f90693efd399be9c17e81383ecb675e98
letsGoThisIsAvailable <shimonzalesch@gmail.com>
2026-04-20 19:53:57 -0400

    initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,31 @@
-module.exports = function(eleventyConfig) {
-  console.log("CONFIG IS LOADING");
+module.exports = function (eleventyConfig) {
 
-  eleventyConfig.addCollection("notes", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/notes/**/*.md");
+  eleventyConfig.addCollection("notes", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/notes/*.md");
   });
 
-  eleventyConfig.addPassthroughCopy({
-    "src/css": "css"
-  });
+  eleventyConfig.addCollection("tags", function(collectionApi) {
+  let files = collectionApi.getFilteredByGlob("src/notes/**/*.md");
+  let tags = {};
+
+  for (let file of files) {
+    let fileTags = file.data.tags || [];
+
+    for (let tag of fileTags) {
+      if (!tags[tag]) {
+        tags[tag] = [];
+      }
+
+      tags[tag].push(file);
+    }
+  }
+
+  console.log("TAGS:", Object.keys(tags));
+
+  return tags;
+});
+
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
 
   return {
     dir: {
